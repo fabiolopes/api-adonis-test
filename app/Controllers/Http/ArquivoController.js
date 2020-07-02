b5e8a247d750d4e7e14fb24f4e131f25f381cb6f
Fabio Bione <fabiolopesbione@hotmail.com>
2020-07-02 16:29:41 -0300

    upload de arquivos

---
+++ app/Controllers/Http/ArquivoController.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const Arquivo = use('App/Models/Arquivo')
+const Tarefa = use('App/Models/Tarefa')
+const Helpers = use('Helpers')
+
+class ArquivoController {
+
+    async create({params, request, response}){
+        try {
+            const tarefa = await Tarefa.findOrFail(params.id)
+
+            const arquivos = request.file('file', {
+                size: '1mb'
+            })
+
+            await arquivos.moveAll(Helpers.tmpPath('arquivos'), file => ({
+                name: `${Date.now()}-${file.clientName}`
+            }))
+
+            if(!arquivos.movedAll()){
+                return arquivos.errors()
+            }
+
+            await Promise.all(
+                arquivos
+                .movedList().map(item => tarefa.arquivos().create({caminho: item.fileName}))
+            )
+
+            return response.status(200).send({message: 'Arquivos inseridos com sucesso'})
+
+        } catch (error) {
+            return response.status(500).send({message: `Ocorreu erro no upload... ${error.message}`})
+        }
+    }
+}
+
+module.exports = ArquivoController
